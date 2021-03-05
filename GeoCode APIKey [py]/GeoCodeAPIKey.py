9299ed8efbc7252d74a3e5428cdc32f5d7b1e3b5
M van Hulzen <mvanhulzen@esri.nl>
2021-03-05 10:10:40 +0100

    Added GeoCode with APIKey in Python

---
+++ GeoCode APIKey [py]/GeoCodeAPIKey.py
@@ -0,0 +1,26 @@
+import requests
+
+#setting up variables
+API_KEY = "AAPKc641339214554353bdd63b00c859cb6f--MMNcOb3mOlcPU7uCwxyL0trs-YCKDrzUkH1Ip0y4AffaV1UDBkDjWFU1e2jyaj"
+address="Koggelaan 33, Zwolle"
+geocode_url = "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
+
+#setting up parameters
+params = {}
+params["f"] = "json"
+params["address"] = address
+params["token"] = API_KEY
+
+#sending request
+r = requests.post(geocode_url,params)
+
+#getting response as python dictionairy from json response
+response = r.json()
+
+#getting x and y from response
+candidate = response["candidates"][0]
+x = candidate["location"]["x"]
+y = candidate["location"]["y"]
+
+#doing something with the response
+print(f"{address} is on x:{x}, y:{y}")
---
+++ GeoCode APIKey [py]/Readme.md
@@ -0,0 +1,5 @@
+# GeoCoding with Python with an API key
+
+Using an API key to GeoCode an address on ArcGIS Platform with Python
+
+![FindLayer](../images/20210305_Screenshot_Geocode_Platform_Python.jpg)<br/>
