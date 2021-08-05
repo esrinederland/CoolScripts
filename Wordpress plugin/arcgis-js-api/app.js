7b0a2cf8c7786477d2245ffc4a8a027c1473a5f4
Mark Jagt <mjagt@users.noreply.github.com>
2021-08-05 16:12:16 +0200

    Code update app.js

--- Wordpress plugin/arcgis-js-api/app.js
+++ Wordpress plugin/arcgis-js-api/app.js
@@ -34,15 +34,6 @@ jQuery(window).load(function () {
          ************************************************************/
         var view = new MapView({
             map: webmap,
-            popup: {
-            dockEnabled: true,
-            dockOptions: {
-                // Disables the dock button from the popup
-                buttonEnabled: false,
-                // Ignore the default sizes that trigger responsive docking
-                breakpoint: false
-            }
-            },
             // IMPORTANT! don't change the container name or make sure it corresponds with the id of the div returned in the file arcgis-js-api.php at line 60
             container: "viewDiv"
         });
